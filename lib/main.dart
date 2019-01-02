c40bf7f26c7188555c2ec86ff399c3df316153d9
许度庆 <xu.duqing@gmail.com>
2019-01-03 00:52:01 +0800

    feat: 添加 Flutter 系统架构图

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter PPT Home Page'),
+      home: MyHomePage(title: 'Flutter System Overview'),
     );
   }
 }
@@ -51,21 +51,226 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Container(
+                  margin: EdgeInsets.all(8),
+                  color: Color.fromRGBO(172,211,188,1),
+                  child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Framework', style: TextStyle(fontWeight: FontWeight.bold),),
+                              Text('Dart')
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    FrameworkWidget(title: 'Material'),
+                                    FrameworkWidget(title: 'Cupertino'),
+                                  ],),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    FrameworkWidget(title: 'Widgets'),
+                                  ],),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    FrameworkWidget(title: 'Rendering'),
+                                  ],),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    FrameworkWidget(title: 'Animation'),
+                                    FrameworkWidget(title: 'Painting'),
+                                    FrameworkWidget(title: 'Gestures'),
+                                  ],),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    FrameworkWidget(title: 'Foundation')
+                                  ],),
+                              ),
+                            ],
+                          ),
+                        )
+
+                  ])
               ),
-              Text(
-                '$_keyCode',
-                style: Theme.of(context).textTheme.display1,
+            ),
+            Expanded(
+              flex: 4,
+              child: new Container(
+                  margin: EdgeInsets.all(8),
+                  color: new Color.fromRGBO(210,222,245	,1),
+                  child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Engine', style: TextStyle(fontWeight: FontWeight.bold),),
+                              Text('C++')
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    EngineWidget(title: 'Service Protocol'),
+                                    EngineWidget(title: 'Composition'),
+                                    EngineWidget(title: 'Platform Channels'),
+                                  ],),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    EngineWidget(title: 'Dart Lsolate Setup'),
+                                    EngineWidget(title: 'Rendering'),
+                                    EngineWidget(title: 'System Events'),
+                                  ],),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    EngineWidget(title: 'Dart VM'),
+                                    EngineWidget(title: 'Frame Scheduling'),
+                                    EngineWidget(title: 'Asset Resolution'),
+                                  ],),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    EngineWidget(title: 'Frame Pipelining'),
+                                    EngineWidget(title: 'Text Layout'),
+                                  ],),
+                              ),
+                            ],
+                          ),
+                        )
+
+                      ])
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              flex: 2,
+              child: new Container(
+                  margin: EdgeInsets.all(8),
+                  color: new Color.fromRGBO(243,227,188,1),
+                  child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Enbedder', style: TextStyle(fontWeight: FontWeight.bold),),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    EmbedderWidget(title: 'Render Surface'),
+                                    EmbedderWidget(title: 'Native Plugins'),
+                                    EmbedderWidget(title: 'Packaging'),
+                                  ],),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: <Widget>[
+                                    EmbedderWidget(title: 'Thread Setup'),
+                                    EmbedderWidget(title: 'Event Loop'),
+                                  ],),
+                              )
+                            ],
+                          ),
+                        )
+
+                      ])
+              ),
+            )
+          ],
         ),
         // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class FrameworkWidget extends StatelessWidget {
+  final String title;
+  const FrameworkWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        alignment: Alignment.center,
+        color: Color.fromRGBO(49,111,56,1),
+        child: Text(title),
+        margin: EdgeInsets.all(5),
+      ),
+    );
+  }
+}
+
+class EngineWidget extends StatelessWidget {
+  final String title;
+  const EngineWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        alignment: Alignment.center,
+        color: Color.fromRGBO(50,	110, 232	,1),
+        child: Text(title),
+        margin: EdgeInsets.all(5),
+      ),
+    );
+  }
+}
+
+class EmbedderWidget extends StatelessWidget {
+  final String title;
+  const EmbedderWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        alignment: Alignment.center,
+        color: Color.fromRGBO(230,	164,	57	,1),
+        child: Text(title),
+        margin: EdgeInsets.all(5),
       ),
     );
   }
