93d6c98201a584f187f1217df0dae071969deaf1
许度庆 <xu.duqing@gmail.com>
2019-01-16 23:33:04 +0800

    fix: rename

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:ppt/home.dart';
+import 'package:ppt/overview.dart';
 
 void main() => runApp(MyApp());
 
